2055d4349a1ec065227899f7587b485f0fcba6ea
francijpg <hellofranciscopg@gmail.com>
2020-09-13 12:48:03 -0300

    Update ListadoNoticias.js

--- src/components/ListadoNoticias.js
+++ src/components/ListadoNoticias.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 
 const ListadoNoticias = ({ noticias }) => (
   <div className="row">
+  {console.log(Array.isArray(noticias) && noticias.length)}
     {noticias.length === 0 || noticias === undefined ? (
       <p className="red darken-4 error white-text center">
         No se encontraron noticias
